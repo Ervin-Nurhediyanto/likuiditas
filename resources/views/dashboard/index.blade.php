b07fcf89c903cfc54480dc116299563f53a667a6
unknown <cafeinai311@gmail.com>
2025-01-03 10:54:20 +0700

    adminLTE

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Likuiditas</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{('assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- IonIcons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{('assets/dist/css/adminlte.min.css')}}">
 </head>
-<body>
-  <div>tes</div>
+
+<body class="hold-transition sidebar-mini">
+  
+  <x-layoutes.dashboard>
+    <x-slot:active>{{ $active }}</x-slot:active>
+    <x-content.dashboard-v3/>
+  </x-layoutes.dashboard>
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{('assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE -->
+<script src="{{('assets/dist/js/adminlte.js')}}"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{('assets/plugins/chart.js/Chart.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{('assets/dist/js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{('assets/dist/js/pages/dashboard3.js')}}"></script>
 </body>
 </html>
